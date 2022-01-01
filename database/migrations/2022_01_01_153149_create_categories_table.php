7db7d1468f0f34d6deb41ded1adc1a664ee9702c
sajede2003 <sajededarzinia@gmail.com>
2022-01-01 23:54:27 +0330

    created add and delete in category

--- database/migrations/2022_01_01_153149_create_categories_table.php
+++ database/migrations/2022_01_01_153149_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Categories extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class Categories extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
+            $table->timestamps();
         });
     }
 
@@ -26,6 +26,6 @@ class Categories extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('categories');
     }
 }
