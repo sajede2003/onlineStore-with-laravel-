233eb2cb77635f480c324decaa35d19242d0ab7d
sajede2003 <sajededarzinia@gmail.com>
2022-01-01 10:38:13 +0330

    created login and fixed errors and logout

--- app/Http/Requests/LoginValidation.php
+++ app/Http/Requests/LoginValidation.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class Validation extends FormRequest
+class LoginValidation extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class Validation extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
@@ -24,11 +24,7 @@ class Validation extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required',
-            'phone_number' => 'required|unique:users|',
-            'email' => 'required|unique:users|email',
-            'password' => 'required|min:8',
-            'confirmPassword' => 'required',
+            //
         ];
     }
 }
---
+++ app/Http/Requests/RegisterValidation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginValidation extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+            'phone_number' => 'required|unique:users|',
+            'email' => 'required|unique:users|email',
+            'password' => 'required|min:8',
+            'confirmPassword' => 'required',
+        ];
+    }
+}
